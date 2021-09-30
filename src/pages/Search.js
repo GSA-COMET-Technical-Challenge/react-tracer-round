b37eddcc9a1fc6422326e198856eaafac46ae337
Mason Donley <mason.donley@inadev.com>
2021-09-30 13:03:46 -0400

    added searchbar placeholder

---
+++ src/pages/Search.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import SearchBar from '../components/SearchBar'
+
+const SearchPage = () => {
+    return (
+        <Style>
+            <div className="content">
+            <SearchBar/>
+            </div> 
+        </Style>
+    )
+}
+
+const Style = styled.div`
+display: flex;
+justify-content: center;
+
+.content {
+    width: 100%;
+    max-width: 1000px;
+    padding: 0.5rem;
+}
+`
+
+export default SearchPage
